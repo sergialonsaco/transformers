aaeb5f2abe58c73798f9b40210c0756767bd16c8
Patrick von Platen <patrick.v.platen@gmail.com>
2020-06-12 11:28:25 +0200

    make style

--- src/transformers/benchmark/benchmark_utils.py
+++ src/transformers/benchmark/benchmark_utils.py
@@ -733,7 +733,9 @@ class Benchmark(ABC):
 
     def print_results(self, result_dict, type_label):
         self.print_fn(80 * "-")
-        self.print_fn("Model Name".center(30) + "Batch Size".center(15) + "Seq Length".center(15) + type_label.center(15))
+        self.print_fn(
+            "Model Name".center(30) + "Batch Size".center(15) + "Seq Length".center(15) + type_label.center(15)
+        )
         self.print_fn(80 * "-")
         for model_name in self.args.model_names:
             for batch_size in result_dict[model_name]["bs"]:
@@ -743,7 +745,11 @@ class Benchmark(ABC):
                         result = str((round(1000 * result) / 1000))
                     else:
                         result = str(result)
-                    self.print_fn(model_name.center(30) + str(batch_size).center(15), str(sequence_length).center(15), result.center(15))
+                    self.print_fn(
+                        model_name.center(30) + str(batch_size).center(15),
+                        str(sequence_length).center(15),
+                        result.center(15),
+                    )
         self.print_fn(80 * "-")
 
     def print_memory_trace_statistics(self, summary: MemorySummary):
